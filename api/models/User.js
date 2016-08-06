1df0fe4d9cac775e112d304594f890cb71c382f9
Valentin “ThisIsMac” M <thisismac47@gmail.com>
2016-08-06 22:16:04 +0200

    added user model

--- api/models/User.js
+++ api/models/User.js
@@ -8,7 +8,60 @@
 module.exports = {
 
   attributes: {
-
+		id : {
+			type: 'number',
+			required: true,
+			unique: true
+		},
+		username: {
+			type: 'string',
+			required: true,
+			unique: true
+		},
+		email: {
+			type: 'string',
+			required: true,
+			unique: true
+		},
+		password: {
+			type: 'string',
+			required: true
+		},
+		money: {
+			type: 'number',
+			defaultsTo: 0.0
+		},
+		role: {
+			type: 'string',
+			defaultsTo: 'USER',
+			in: ['USER', 'MOD', 'ADMIN', 'FOUNDER']
+		},
+		developer: {
+			type: 'boolean',
+			defaultsTo: false,
+			boolean: true
+		},
+		ip: {
+			type: 'string',
+			ipv4: true
+		},
+		lang: {
+			type: 'string',
+			defaultsTo: 'fr'
+		},
+		created: {
+			type: 'string',
+			datetime: true
+		},
+		modified: {
+			type: 'string',
+			datetime: true
+		},
+		state: {
+			type: 'string',
+			defaultsTo: 'UNCONFIRMED',
+			in: ['CONFIRMED', 'UNCONFIRMED']
+		}
   }
 };
 
