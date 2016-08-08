8624c3a49e26d686cded712e3de078ac6ef855d4
Valentin “ThisIsMac” M <thisismac47@gmail.com>
2016-08-09 00:41:55 +0200

    updated model

--- api/models/Plugin.js
+++ api/models/Plugin.js
@@ -8,7 +8,61 @@
 module.exports = {
 
   attributes: {
-		
+		id: {
+			type: 'number',
+			required: true,
+			unique: true,
+    	autoIncrement: true,
+    	primaryKey: true,
+		},
+		title: {
+			type: 'string',
+			unique: true,
+			required: true,
+		},
+		name: {
+			type: 'string',
+			unique: true,
+			required: true
+		},
+		author: {
+			model: 'User',
+			required: true
+		},
+		description: {
+			type: 'string',
+			required: true
+		},
+		img: {
+			type: 'string',
+			url: true,
+			required: true
+		},
+		version: {
+			type: 'string',
+			is: "^(\d+\.)?(\d+\.)?(\d+)$",
+			required: true
+		},
+		dependencies: {
+			type: 'json',
+			defaultsTo: { 'CMS': "1.0.0" }
+		},
+		official: {
+			type: 'boolean',
+			defaultsTo: false
+		},
+		created: {
+			type: 'datetime',
+			defaultsTo: Date.now()
+		},
+		downloads: {
+			type: 'number',
+			defaultTo: 0
+		},
+		price: {
+			type: 'number',
+			defaultsTo: 0
+		}
   }
 };
 
