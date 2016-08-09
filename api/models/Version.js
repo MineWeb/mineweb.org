f79530adb7549d112cc3e9b916f233874dcf4dd5
Valentin <contact@eywek.fr>
2016-08-09 16:55:05 +0200

    Edit version model

--- api/models/Version.js
+++ api/models/Version.js
@@ -9,6 +9,45 @@ module.exports = {
 
   attributes: {
 
+    id : {
+			type: 'integer',
+			unique: true,
+    	autoIncrement: true,
+    	primaryKey: true,
+		},
+
+    version : {
+      type: 'string',
+      required: true,
+      unique: true,
+      regex: /^(\d+\.)?(\d+\.)?(\*|\d+)$/
+    },
+
+    type: {
+      type: 'string',
+      in: ['CHOICE', 'FORCED'],
+      defaultsTo: 'CHOICE'
+    },
+
+    visible: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
+    is: {
+      type: 'string',
+      in: ['SNAPSHOT', 'RELEASE'],
+      defaultsTo: 'SNAPSHOT'
+    },
+
+    releaseDate: {
+			type: 'datetime'
+		},
+
+    changelog: {
+      type: 'json',
+      defaultsTo: null
+    }
+
   }
 };
-
