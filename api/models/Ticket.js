16b8d8392b02eea765ad06fd84e1470128b89bcf
Valentin <contact@eywek.fr>
2016-08-09 17:10:24 +0200

    Edit ticket model

--- api/models/Ticket.js
+++ api/models/Ticket.js
@@ -8,7 +8,60 @@
 module.exports = {
 
   attributes: {
-		
+
+    id: {
+			type: 'integer',
+			unique: true,
+    	autoIncrement: true,
+    	primaryKey: true,
+		},
+
+    user: {
+			model: 'User',
+			required: true
+		},
+
+    license: {
+      model: 'License',
+      defaultsTo: null
+    },
+
+    hosting: {
+      model: 'Hosting',
+      defaultsTo: null
+    },
+
+    title: {
+      type: 'string',
+      required: true
+    },
+
+    category: {
+      type: 'string',
+      required: true,
+      in: ['GENERAL', 'SERVER', 'DEVELOPMENT', 'SUGGESTION', 'QUESTION']
+    },
+
+    content: {
+      type: 'string',
+      required: true
+    },
+
+    state: {
+      type: 'string',
+      in: ['WAITING_STAFF_RESPONSE', 'WAITING_USER_RESPONSE', 'CLOSED'],
+      defaultsTo: 'WAITING_STAFF_RESPONSE'
+    },
+
+    supported: {
+      model: 'User',
+      defaultsTo: null
+    },
+
+    closedDate: {
+      type: 'datetime',
+      defaultsTo: null
+    }
+
   }
 };
-
