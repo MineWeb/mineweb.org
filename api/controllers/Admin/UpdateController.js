7faf6bdcd162dbe6d0d9057520c062c6bd5ba3c8
Valentin <contact@eywek.fr>
2016-10-04 19:10:10 +0200

    Handle update add (admin)

--- api/controllers/Admin/UpdateController.js
+++ api/controllers/Admin/UpdateController.js
@@ -27,7 +27,64 @@ module.exports = {
   },
 
   add: function (req, res) {
+    RequestManagerService.setRequest(req).setResponse(res).valid({
+			"Tous les champs ne sont pas remplis ou mal remplis.": [
+				['version', ''],
+				['type', ''],
+        {
+          field: 'type',
+          in: ['CHOICE', 'FORCED'],
+          error: ''
+        },
+        ['state', ''],
+        {
+          field: 'state',
+          in: ['STAGING', 'DEVELOPMENT', 'SNAPSHOT', 'RELEASE'],
+          error: ''
+        },
+        ['visible', ''],
+        {
+          field: 'visible',
+          in: [true, false],
+          error: ''
+        }
+			]
+		}, function () {
 
+      // handle changelog
+      var changelog = []
+      for (var index in req.body.changelog) {
+        changelog.push(req.body.changelog[index])
+      }
+
+      // update
+      Version.create({
+        version: req.body.version,
+        type: req.body.type,
+        state: req.body.state,
+        visible: req.body.visible,
+        releaseDate: (req.body.releaseDate) ? req.body.releaseDate : null,
+        changelog: {
+          "fr_FR": changelog
+        }
+      }).exec(function (err, versionUpdated) {
+
+        if (err) {
+          sails.log.error(err)
+          return res.serverError()
+        }
+
+        NotificationService.success(req, req.__('La version a bien été enregistrée !'))
+
+        res.json({
+          status: true,
+          msg: req.__('La version a bien été enregistrée !'),
+          inputs: {}
+        })
+
+      })
+
+    })
   },
 
   edit: function (req, res) {
@@ -57,7 +114,7 @@ module.exports = {
         type: req.body.type,
         state: req.body.state,
         visible: req.body.visible,
-        releaseDate: req.body.releaseDate,
+        releaseDate: (req.body.releaseDate) ? req.body.releaseDate : null,
         changelog: {
           "fr_FR": changelog
         }
