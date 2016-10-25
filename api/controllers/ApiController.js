c97ff861ed42be5f70ad41f67a2a3a43ae1bd4af
Valentin <contact@eywek.fr>
2016-10-25 17:03:18 +0200

    Fix

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -5,15 +5,17 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-var httpProxy = require('http-proxy');
-var proxy     = httpProxy.createProxyServer();
+var http = require('http')
+var httpProxy = require('http-proxy')
+var keepAliveAgent = new http.Agent({keepAlive: true})
+var proxy = httpProxy.createProxyServer({target: sails.config.api.endpointWithoutVersion, agent: keepAliveAgent})
 
 module.exports = {
 
   forward: function (req, res) {
     return proxy.web(req, res, {
       target: sails.config.api.endpointWithoutVersion
-    });
-  },
+    })
+  }
 
 }
