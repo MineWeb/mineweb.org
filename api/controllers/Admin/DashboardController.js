6dacbd763f41367e94b5208c9da9c535b5c641d6
Eywek <Eywek@users.noreply.github.com>
2017-01-19 20:03:03 +0100

    Edit total stats (only licences + hosting)

--- api/controllers/Admin/DashboardController.js
+++ api/controllers/Admin/DashboardController.js
@@ -62,18 +62,18 @@ module.exports = {
         async.parallel([
           // Find with PayPal
           function (next) {
-            PayPalHistory.query("SELECT SUM(paymentAmount - taxAmount) AS amount FROM paypalhistory WHERE state = 'COMPLETED'", function (err, profit) {
+            PayPalHistory.query("SELECT SUM(paypal.paymentAmount - paypal.taxAmount) AS sum FROM paypalhistory AS paypal INNER JOIN purchase AS purchase ON purchase.id = paypal.purchase WHERE (purchase.type = 'LICENSE' OR purchase.type = 'HOSTING') AND MONTH(paypal.paymentDate) = MONTH(NOW()) AND paypal.state = 'COMPLETED'", function (err, profit) {
               if (err)
                 return next(err)
-              next(null, profit[0].amount)
+              next(null, profit[0].sum)
             })
           },
           // Find with Dedipass
           function (next) {
-            DedipassHistory.find().limit(1).sum('payout').exec(function (err, profit) {
+            DedipassHistory.query("SELECT SUM(dedipass.payout) AS sum FROM dedipasshistory AS dedipass INNER JOIN purchase AS purchase ON purchase.id = dedipass.purchase WHERE (purchase.type = 'LICENSE' OR purchase.type = 'HOSTING') AND MONTH(dedipass.createdAt) = MONTH(NOW())", function (err, profit) {
               if (err)
                 return next(err)
-              next(null, profit[0].payout)
+              next(null, profit[0].sum)
             })
           }
         ], function (err, results) {
