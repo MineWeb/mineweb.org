0ab6e06a943fb311ec448113589e7a5452fdb82c
Valentin <contact@eywek.fr>
2017-09-04 16:08:41 +0200

    Display supported/requirements

--- api/controllers/MarketController.js
+++ api/controllers/MarketController.js
@@ -9,6 +9,43 @@ var async			= require('async');
 
 module.exports = {
 
+  getVersionAndOperator: function(string) {
+    var operator = '='
+    var version = string
+    if (string.split(' ').length > 1) {
+      operator = string.split(' ')[0]
+      version = string.split(' ')[1]
+    } else {
+      var regex = /(\^|>=|<=|=|>|<|~|)([0-9]+\.[0-9]+\.[0-9]+)/g
+      var matches = regex.exec(version)
+      if (matches.length === 3) {
+        version = matches[2]
+        operator = matches[1]
+        var split
+        if (operator === '^') {
+          operator = '>='
+          split = [
+            (parseInt(version.split('.')[0]) + 1),
+            version.split('.')[1],
+            version.split('.')[2]
+          ]
+          version += ' (< ' + split.join('.') + ')'
+        } else if (operator === '~') {
+          operator = '>='
+          split = [
+            version.split('.')[0],
+            (parseInt(version.split('.')[1]) + 1),
+            version.split('.')[2]
+          ]
+          version += ' (< ' + split.join('.') + ')'
+        }
+      }
+    }
+    return [version, operator]
+    // Caret operator: ^1.0 can be written as >=1.0.0 <2.0.0
+    // Tilde operator: ~1.0.0 can be written as >=1.0.0 <1.1.0
+  },
+
 	/**
 	 * Render the index view of the marketplace
 	 */
@@ -95,7 +132,7 @@ module.exports = {
 	 * Render a theme page
 	 */
 	theme: function (request, response) {
-
+    var self = this
     var slug = request.params.slug;
     if (slug === undefined)
       return response.notFound();
@@ -133,11 +170,11 @@ module.exports = {
 			}
 
 			// each
-			async.forEach(supportedArray, function (requirement, callback) {
+			async.forEach(supportedArray, function (supported, callback) {
 
-				var name = Object.keys(requirement)[0]
+				var name = Object.keys(supported)[0]
 
-				if (name != "CMS" && name != undefined && supported[name] != undefined) {
+				if (name !== "CMS" && name !== undefined && supported[name] !== undefined) {
 
 					var model = Plugin
 					var id = name // author.slug.id
@@ -153,16 +190,13 @@ module.exports = {
 							sails.log.error(err)
 						}
 						else if (item !== undefined) {
-
-							var operator = (supported[name].split(' ').length > 1) ? supported[name].split(' ')[0] : '='
-							var version = (supported[name].split(' ').length > 1) ? supported[name].split(' ')[1] : supported[name]
-
+              var versionAndOperator = self.getVersionAndOperator(supported[name])
 							name = '<a href="/market/plugin/'+item.slug+'">'+item.name+'</a>'
 
 							result.push({
 								name: name,
-								operator: operator,
-								version: version
+								operator: versionAndOperator[1],
+								version: versionAndOperator[0]
 							})
 							callback()
 
@@ -173,16 +207,15 @@ module.exports = {
 					})
 
 				}
-				else if(name == "CMS") {
+				else if(name === "CMS") {
 
-					var operator = (supported[name].split(' ').length > 1) ? supported[name].split(' ')[0] : '='
-					var version = (supported[name].split(' ').length > 1) ? supported[name].split(' ')[1] : supported[name]
+          var versionAndOperator = self.getVersionAndOperator(supported[name])
 
-					result.push({
-						name: 'CMS',
-						operator: operator,
-						version: version
-					})
+          result.push({
+            name: 'CMS',
+            operator: versionAndOperator[1],
+            version: versionAndOperator[0]
+          })
 					callback()
 
 				}
@@ -213,7 +246,7 @@ module.exports = {
 	 * Render a plugin page
 	 */
 	plugin: function (request, response) {
-
+    var self = this
     var slug = request.params.slug;
     if (slug === undefined)
       return response.notFound();
@@ -276,15 +309,13 @@ module.exports = {
 						}
 						else if (item !== undefined) {
 
-							var operator = (requirements[name].split(' ').length > 1) ? requirements[name].split(' ')[0] : '='
-							var version = (requirements[name].split(' ').length > 1) ? requirements[name].split(' ')[1] : requirements[name]
-
+              var versionAndOperator = self.getVersionAndOperator(requirements[name])
 							name = '<a href="/market/'+type+'/'+item.slug+'">'+item.name+'</a>'
 
 							result.push({
 								name: name,
-								operator: operator,
-								version: version
+								operator: versionAndOperator[1],
+								version: versionAndOperator[0]
 							})
 							callback()
 
@@ -297,13 +328,11 @@ module.exports = {
 				}
 				else if(name == "CMS") {
 
-					var operator = (requirements[name].split(' ').length > 1) ? requirements[name].split(' ')[0] : '='
-					var version = (requirements[name].split(' ').length > 1) ? requirements[name].split(' ')[1] : requirements[name]
-
+          var versionAndOperator = self.getVersionAndOperator(requirements[name])
 					result.push({
 						name: 'CMS',
-						operator: operator,
-						version: version
+						operator: versionAndOperator[1],
+						version: versionAndOperator[0]
 					})
 					callback()
 
